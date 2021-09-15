fc9471e6b1eeac6f13ee66d107c648b85314b6b5
whiteashes <francesco.guarneri@yahoo.com>
2021-09-15 09:29:54 +0200

    2nd POST to AEC server

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect, flash, make_response, url_for
+from typing import NamedTuple
+from cv2 import namedWindow
+from flask import Flask, render_template, session, request, redirect, flash, make_response, url_for
 import os
 import json
 import bcModules
+import requests
+import urllib.request
 
 app = Flask(__name__)
 
@@ -16,6 +20,16 @@ def hello():
         return redirect(url_for('upload'))
 
     return render_template('index.html')
+
+
+@app.route('/welcome',methods=['GET','POST'])
+def welcome(msg=None):
+
+    if request.method == 'POST':
+        return redirect(url_for('hello'))
+
+    return render_template('welcome.html'), {"Refresh":"1; url=/"}
+
 
 @app.route('/upload',methods=['GET','POST'])
 def upload():
@@ -32,16 +46,34 @@ def upload():
             finalPath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
             file.save(finalPath)
             
-            path=bcModules.extraction(finalPath)
+            #json parsed file+namefile
+            body,name=bcModules.extraction(finalPath)
 
-            fp=open(path,'r')
+            fp=open(name,'r')
             fp.seek(0)
             f=fp.read()
 
+            #1st POST to localhost
             res = make_response(f)
             res.headers['Content-Type'] = 'application/json'
 
-            return res
+            print(body)
+            print(name)
+
+            #2nd POST to server
+            url = "http://192.168.46.70:7478/aec/api/MesExecute"
+            req = urllib.request.Request(url)
+            req.add_header('Content-Type','application/json; charset=utf-8')
+            jsondata = json.dumps(body)
+            jsonbytes = jsondata.encode('utf-8')
+
+            req.add_header('Authorization','Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqZXRzb24iLCJzaG9ydEV4cCI6MTc4OTE5OTA0MH0.lD5ELumFc6LXyfb7A7FFqTYdgBxivIGO17zDWe5E1nHh-XQEuKrwPCsOAIEK4oEcYYuXRFUBy9TtMH27RrRciw')
+
+            response = urllib.request.urlopen(req,jsonbytes)
+            print("Status code: %s" %response.getcode())
+
+            return redirect(url_for('welcome'))
+
 
     return render_template('upload.html')
 
--- bcModules.py
+++ bcModules.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import pytesseract as pytes
 from pytesseract import Output
+import easyocr
 import time,os,re
 from re import A, search
 from os import listdir
@@ -33,21 +34,22 @@ def cvProcessing(frameName):
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
     #noise removal
-    blurred = cv.fastNlMeansDenoising(gray,50,7,21)
+    
+    #blurred = cv.fastNlMeansDenoising(gray,50,7,21)
 
     #threshold ( tresh: output img )
     #first arg MUST BE a grayscaled img ; second arg : max pixel value
     #third arg : adaptive threshold type (mean or gaussian) ; fourth arg: threshold type (ONLY binary for adaptive)
     #fifth arg: pixel block size ; 0 black 255 white (max value)
-    thresh = cv.adaptiveThreshold(blurred,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,17,1.8)
+    #thresh = cv.adaptiveThreshold(blurred,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,17,1.8)
 
     #histogram equalization -> improve contrast
-    histed = cv.equalizeHist(thresh)
+    #histed = cv.equalizeHist(thresh)
 
     #
 
     frameNameFinal = frameName+"__thresh"+str(time.time())+".jpg"
-    cv.imwrite(frameNameFinal,histed) #save thresholded img
+    cv.imwrite(frameNameFinal,gray) #save thresholded img
     print("Img saved: %s" %frameNameFinal)
 
     img = cv.imread(frameNameFinal)
@@ -57,6 +59,10 @@ def cvProcessing(frameName):
 def callPyTes(img):
     return pytes.image_to_data(img, output_type=Output.DICT,config='--psm 11 -c tessedit_do_invert=0',lang='ita',)
 
+
+#def callEasyOCR(img):
+    reader = easyocr.Reader(['en'])
+    return reader.readtext(img,detail=False,paragraph=True)
 
 def openAndWrite(d):
     file=open("detected.txt","r+")
@@ -85,27 +91,17 @@ def capturing(cap):
 
     return frameName
 
-#jsoning dicts
-def jsoned(card):
-    parsed=card['name']+card['surname']+".json"
-    with open(parsed,'w') as fp:
-        json.dump(card,fp)
-    
-    return parsed
-
 def regexFind(file):
     mailPattern = '\S+@\S+'
     namePattern = "([A-Z][a-z]*)([\\s\\\'-][A-Z][a-z]*)*"
 
-    card =	{
-        "name": "",
-        "surname": "",
-        "company": "",
-        "mail": "",
-        "website": "",
-        "piva": "",
-        "phone": ""
-    }
+    card = {"Type": "itemnote_create", "Context": "OP", 
+            "note": "", "description": "", "type":  "visitor","station":""}
+
+    #tmp
+    ns = {"name": "","surname":""}
+
+    opt = {"piva":"","phone":"","website":""}
 
     #for more accuracy with names
     titles = ['Dott.','Ing.','Dott.ssa','Avv.']
@@ -121,26 +117,26 @@ def regexFind(file):
     for i in range(len(arr)):
 
         if re.search("P.IVA",arr[i]) or re.search("P IVA",arr[i]) or re.search("PIVA",arr[i]):
-            card['piva'] = arr[i+1]
+            opt['piva'] = arr[i+1]
         
         #searching for +39
         if arr[i]=="+39":
             if arr[i+1]==" " or len(arr[i+1])<6:
-                card['phone'] = "+39"+arr[i+2]
+                opt['phone'] = "+39"+arr[i+2]
             else:
-                card['phone'] = "+39"+arr[i+1]
+                opt['phone'] = "+39"+arr[i+1]
 
         if re.search("www",arr[i],re.IGNORECASE) or re.search("https",arr[i],re.IGNORECASE) or re.search("http",arr[i],re.IGNORECASE):
-            card['website'] = arr[i].lower()
+            opt['website'] = arr[i].lower()
 
         for x in range(len(titles)):
             if re.search(titles[x],arr[i]):
-                card['name'] = arr[i+1]
+                ns['name'] = arr[i+1]
 
                 if(len(arr[i+2])<=4):
-                    card['surname'] = arr[i+2] + " " + arr[i+3]
+                    ns['surname'] = arr[i+2] + " " + arr[i+3]
                 else:
-                    card['surname'] = arr[i+2]
+                    ns['surname'] = arr[i+2]
             x=x+1
         i=i+1
 
@@ -153,15 +149,14 @@ def regexFind(file):
         exit()
 
     #mail
-    card['mail'] = m[0]
-    card['mail'] = card['mail'].replace(':','.')
+    card['note'] = m[0]
+    card['note'] = card['note'].replace(':','.')
 
     #company by mail
-    card['company'] = card['mail'].split("@")[1].split(".")[0].capitalize()
+    card['station'] = card['note'].split("@")[1].split(".")[0].capitalize()
     
-
     #if it did not find name by titles
-    if card['name']=="" or card['surname']=="":
+    if ns['name']=="" or ns['surname']=="":
         
         #regex matching between name pattern and first line
         n=re.findall(namePattern,firstLine)
@@ -183,18 +178,23 @@ def regexFind(file):
             else:
                 secondTElem.append('X')
 
-        stringName = card['mail'].split("@")[0]
+        stringName = card['note'].split("@")[0]
         i=0
         for x in firstTElem:
             if re.search(x,stringName,re.IGNORECASE) and len(x)>2:
-                card['name'] = x.replace(" ","")
-                card['surname'] = secondTElem[i].replace(" ","")
+                ns['name'] = x.replace(" ","")
+                ns['surname'] = secondTElem[i].replace(" ","")
             i=i+1
+
+    card['description'] = ns['name']+" "+ns['surname']
 
     print(firstTElem)
     print(secondTElem)
 
-    return card
+    with open(ns['name']+ns['surname']+".json",'w') as fp:
+        json.dump(card,fp)
+
+    return card,ns['name']+ns['surname']+".json"
 
 def extraction(frameName):
     #WINDOWS ONLY,keep comment if you use UNIX like OS#
@@ -209,17 +209,15 @@ def extraction(frameName):
 
     #dictionary
     d=callPyTes(img)
-    print(d.keys())
+    #print(d.keys())
 
     file=openAndWrite(d)
-    card=regexFind(file)
-    parsed=jsoned(card)
+    card,name=regexFind(file)
 
-    print("jsoned %s" %parsed)
+    print("jsoned")
     print(card)
 
-    file.close()
-    return parsed
+    return card,name
 
 
 # service functions #
